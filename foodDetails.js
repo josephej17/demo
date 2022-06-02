6c0828b6afb88b8b09ff42b76441bcd01b21b873
josephej17 <53663811+josephej17@users.noreply.github.com>
2022-06-02 10:09:04 -0500

    Update foodDetails.js

--- foodDetails.js
+++ foodDetails.js
@@ -2,10 +2,10 @@
   {   
         "id": 1,
         "src": "require('./../assets/menu/brownie.jpg')",
-        "name":'Brownie',
-        "description":'Pudín de chocolate con trufas',
+        "name":"Brownie",
+        "description":"Pudín de chocolate con trufas",
         "price": 1700,
-        "restaurant": 'El gran pudini',
+        "restaurant": "El gran pudini",
         "restaurantImg": "require('./../assets/restaurant/res6.jpg')",
         "quantity": 1
     }
