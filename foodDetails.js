84d567425344c7f116231820a5da53885c40c575
josephej17 <53663811+josephej17@users.noreply.github.com>
2022-06-02 10:18:16 -0500

    Create foodDetails.js

---
+++ foodDetails.js
@@ -0,0 +1,10 @@
+[
+  {   
+        "id": 1,
+        "name":"Brownie",
+        "description":"Pudín de chocolate con trufas",
+        "price": 1700,
+        "restaurant": "El gran pudini",
+        "quantity": 1
+    }
+]
