91ad6deffdf03e8a51b9cdd10ef0dbbee447aff8
josephej17 <53663811+josephej17@users.noreply.github.com>
2022-06-02 09:58:57 -0500

    Update foodDetails.js

--- foodDetails.js
+++ foodDetails.js
@@ -1,42 +1,42 @@
 [
   {   
-        id: 1,
-        src: require("./../assets/menu/brownie.jpg"),
-        name:'Brownie',
-        description:'Pudín de chocolate con trufas',
-        price: 1700,
-        restaurant: 'El gran pudini',
-        restaurantImg: require("./../assets/restaurant/res6.jpg"),
-        quantity: 1
+        "id": 1,
+        "src": require("./../assets/menu/brownie.jpg"),
+        "name":'Brownie',
+        "description":'Pudín de chocolate con trufas',
+        "price": 1700,
+        "restaurant": 'El gran pudini',
+        "restaurantImg": require("./../assets/restaurant/res6.jpg"),
+        "quantity": 1
     },
     {
-        id: 2,
-        src: require("./../assets/menu/cheeseCake.jpg"),
-        name:'Cheese Cake',
-        description:'Delicioso postre de queso + galletas',
-        price: 2000,
-        restaurant: 'El gran pudini',
-        restaurantImg: require("./../assets/restaurant/res6.jpg"),
-        quantity: 1
+        "id": 2,
+        "src": require("./../assets/menu/cheeseCake.jpg"),
+        "name":'Cheese Cake',
+        "description":'Delicioso postre de queso + galletas',
+        "price": 2000,
+        "restaurant": 'El gran pudini',
+        "restaurantImg": require("./../assets/restaurant/res6.jpg"),
+        "quantity": 1
     },
     {
-        id: 3,
-        src: require("./../assets/menu/chocoShake.jpg"),
-        name:'Postre napolitano de chocolate',
-        description:'Brownie + chocolate',
-        price: 6500,
-        restaurant: 'El gran pudini',
-        restaurantImg: require("./../assets/restaurant/res6.jpg"),
-        quantity: 1
+        "id": 3,
+        "src": require("./../assets/menu/chocoShake.jpg"),
+        "name":'Postre napolitano de chocolate',
+        "description":'Brownie + chocolate',
+        "price": 6500,
+        "restaurant": 'El gran pudini',
+        "restaurantImg": require("./../assets/restaurant/res6.jpg"),
+        "quantity": 1
     },
     {
-        id: 4,
-        src: require("./../assets/menu/chineseSamosa.jpg"),
-        name:'Lumpias chinas',
-        description:'Camarón, guizantes',
-        price:2300,
-        restaurant: 'XIQUEN Huan',
-        restaurantImg: require("./../assets/restaurant/res5.jpg"),
-        quantity: 1
+        "id": 4,
+        "src": require("./../assets/menu/chineseSamosa.jpg"),
+        "name":'Lumpias chinas',
+        "description":'Camarón, guizantes',
+        "price":2300,
+        "restaurant": 'XIQUEN Huan',
+        "restaurantImg": require("./../assets/restaurant/res5.jpg"),
+        "quantity": 1
     }
 ]
